ffd06c2eee3914fb9a8927edc7effe9c40d6b700
Titov Ihor <44226375+lurak@users.noreply.github.com>
2020-05-30 21:36:38 +0300

    New table

--- generate.py
+++ generate.py
@@ -2,17 +2,66 @@ import random
 
 
 def gen(v_row_1, v_row_2, v_column_1, v_column_2):
-    table = [[0 for i in range(10)] for j in range(10)]
-    for i in range(len(v_row_1)):
-        choice_1 = random.randint(1,len(table[0])-1)
-        choice_2 = random.randint(1,len(table[0])-1)
+    table = [[0 for i in range(300)] for j in range(300)]
+    lst_1 = list()
+    lst_2 = list()
+    i = 0
+    while True:
+        if i == (len(v_row_1) - 1):
+            i = 0
+        choice_1 = random.randint(1, len(table[0])-1)
+        choice_2 = random.randint(1, len(table[0])-1)
+        while True:
+            if choice_1 in lst_1:
+                choice_1 += 1
+            elif choice_1 == (len(table) - 1):
+                choice_1 = 1
+            else:
+                break
+        while True:
+            if choice_2 in lst_2:
+                choice_2 += 1
+            elif choice_2 == (len(table) - 1):
+                choice_2 = 1
+            else:
+                break
         table[0][choice_1] = v_row_1[i]
         table[len(table) - 1][choice_2] = v_row_2[i]
-    for i in range(len(v_column_1)):
-        choice_1 = random.randint(1,len(table)-1)
-        choice_2 = random.randint(1,len(table)-1)
+        lst_1.append(choice_1)
+        lst_2.append(choice_2)
+        i += 1
+        if len(lst_1) == (len(table[0]) - 2):
+            break
+    i = 0
+    lst_1 = list()
+    lst_2 = list()
+    while True:
+        if i == (len(v_row_1) - 1):
+            i = 0
+        choice_1 = random.randint(1, len(table[0])-1)
+        choice_2 = random.randint(1, len(table[0])-1)
+        while True:
+            if choice_1 in lst_1:
+                choice_1 += 1
+            elif choice_1 == (len(table) - 1):
+                choice_1 = 1
+            else:
+                break
+        while True:
+            if choice_2 in lst_2:
+                choice_2 += 1
+            elif choice_2 == (len(table) - 1):
+                choice_2 = 1
+            else:
+                break
         table[choice_1][0] = v_column_1[i]
         table[choice_2][len(table) - 1] = v_column_2[i]
+        lst_1.append(choice_1)
+        lst_2.append(choice_2)
+        i += 1
+        if len(lst_1) == (len(table) - 2):
+            break
+
     with open("table.txt", 'w') as f:
         for i in range(len(table)):
             line = str()
@@ -25,4 +74,4 @@ def gen(v_row_1, v_row_2, v_column_1, v_column_2):
 
 
 if __name__ == "__main__":
-    gen([40,100,1,8], [0,0,0,0], [0,8,5,54,0,0], [0,0,0,0,0,0])
+    gen([40,100,1,8], [8,5,9,42], [100,8,5,54,98,56], [22,77,15,4,9,52])
