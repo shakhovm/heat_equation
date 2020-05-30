c096cc11102aa1b96769636aba98e7f91369ac56
smaxym <02smaxym@gmail.com>
2020-05-30 17:26:31 +0300

    fix

--- main.cpp
+++ main.cpp
@@ -19,10 +19,31 @@ inline bool von_neumann_criterion(const T& delta_x, const T& delta_y, const T& d
                       * density * capacity / conduction;
 }
 
-Array2D jpeg_colormap(const Array2D &heat_matrix, double max_value);
 template <typename T>
 std::tuple<JSAMPLE, JSAMPLE, JSAMPLE> heatmap_color(const T& value,
         const T& min_val, const T& max_val);
+
+template <typename T>
+Array2D redistribute_heat(Array2D &plate_matrix, const T& delta_x, const T& delta_y, const T& delta_t,
+                          const T& conduction, const T& density, const T& capacity)
+{
+    size_t rows = plate_matrix.get_height(),
+            cols = plate_matrix.get_width();
+    double delta_x_sq = delta_x * delta_x,
+            delta_y_sq = delta_y * delta_y,
+            phys_params = conduction / (density * capacity);
+    Array2D plate_buffer = plate_matrix;
+    for (size_t row = 1; row < rows - 1; ++row)
+    {
+        for (size_t col = 1; col < cols - 1; ++col)
+        {
+            double laplasian_x = (plate_matrix(row, col - 1) - 2 * plate_matrix(row, col) + plate_matrix(row, col + 1)) / delta_x_sq,
+                    laplasian_y = (plate_matrix(row - 1, col) - 2 * plate_matrix(row, col) + plate_matrix(row + 1, col)) / delta_y_sq;
+            plate_buffer(row, col) = plate_matrix(row, col) + delta_t * phys_params * (laplasian_x + laplasian_y);
+        }
+    }
+    return plate_buffer;
+}
 
 int main(int argc, char* argv[])
 {
@@ -40,32 +61,6 @@ int main(int argc, char* argv[])
 
 
 
-    auto color = heatmap_color(0, 0, 100);
-    std::cout << +std::get<0>(color) << " " << +std::get<1>(color) << " " << +std::get<2>(color) << std::endl;
-
-
-
-
-    if (!von_neumann_criterion(delta_x, delta_y, delta_t, temp_conduct, density, temp_capacity))
-    {
-        std::cout << "von Neumann criterion is not satisfied" << std::endl;
-        return 0;
-    }
-
-    for (double cur_time = 0; cur_time < end_time; cur_time += delta_t)
-    {
-        plate_buffer = plate_matrix;
-        for (size_t row = 1; row < rows - 1; ++row)
-        {
-            for (size_t col = 1; col < cols - 1; ++col)
-            {
-                double laplasian_x = (plate_buffer(row, col - 1) - 2 * plate_buffer(row, col) + plate_buffer(row, col + 1)) / delta_x_sq,
-                        laplasian_y = (plate_buffer(row - 1, col) - 2 * plate_buffer(row, col) + plate_buffer(row + 1, col)) / delta_y_sq;
-                plate_matrix(row, col) = plate_buffer(row, col) + delta_t * phys_params * (laplasian_x + laplasian_y);
-            }
-        }
-        plate_matrix.print();
-    }
 }
 
 template <typename T>
