90cba382a7f9cfde36fea2c8a2d50e5c8492c159
shakhovm <shakhov@ucu.edu.ua>
2020-05-30 13:31:53 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <boost/mpi.hpp>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    boost:: mpi::environment env{argc, argv};
+    boost:: mpi::communicator world;
+    std :: cout << world.rank() << ", " << world.size() << std::endl;
+}
